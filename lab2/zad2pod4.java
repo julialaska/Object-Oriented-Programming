f4f6e4161a1f811d56f2545fca3a00948abd96cd
laska.julia <julkalas101@gmail.com>
2021-10-16 21:09:45 +0200

    commit

--- lab2/zad2pod4.java
+++ lab2/zad2pod4.java
@@ -1,4 +1,3 @@
-import java.util.ArrayList;
 import java.util.Scanner;
 
 public class zad2pod4 {
@@ -7,27 +6,28 @@ public class zad2pod4 {
         System.out.println("Podaj n");
         int n = scan.nextInt();
 
-
-        ArrayList<Integer> lista = new ArrayList<>();
-
-        for(int i = 0; i < n; i++){
-            System.out.println("Podaj liczbę");
-            int liczba = scan.nextInt();
-            lista.add(liczba);
-        }
-
-        for(Integer z: lista){
-            System.out.println(z);
-            int max = z;
-            if
-        }
+        int [] tab = new int[n];
 
         for(int i = 0; i < n; i++){
             System.out.println("Podaj liczbę");
-            int liczba = scan.nextInt();
-            int max = liczba;
-            if(liczba)
+            tab[i] = scan.nextInt();
+        }
+
+        int max = tab[0];
+        int min = tab[0];
+
+        for(int i = 0; i < n; i++){
+            if(tab[i] > max)
+                max = tab[i];
 
         }
+
+        for(int i = 0; i < n; i++){
+            if(tab[i] < min)
+                min = tab[i];
+
+        }
+        System.out.println("max " + max);
+        System.out.println("min " + min);
     }
 }
